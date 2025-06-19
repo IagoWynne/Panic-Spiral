e2d65c7f612ad48fe85239e02e50b335e6265100
James Richardson <iagowynne@gmail.com>
2025-06-19 16:50:40 +0100

    feat[website|api]: add leaderboard page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import PanicSpiralGame from "./game";
 import { useRef } from "react";
 
@@ -8,18 +7,8 @@ export default function Home() {
   const gameParentRef = useRef(null);
 
   return (
-    <div className="grid grid-cols-[64px_1fr_64px] grid-rows-[64px_1fr] gap-4 items-center justify-center">
-      <Image
-        // className="dark:invert"
-        src="/panic-spiral.jpg"
-        alt="Panic Spiral Logo"
-        width={64}
-        height={64}
-        priority
-      />
-      <div className="row-start-2 col-start-2 max-w-[1200px]" ref={gameParentRef}>
+      <div ref={gameParentRef}>
         <PanicSpiralGame parentRef={gameParentRef}/>
       </div>
-    </div>
   );
 }
