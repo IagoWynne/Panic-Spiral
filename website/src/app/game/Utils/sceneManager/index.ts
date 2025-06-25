a8a4c4d07a9cdba2fd81ecd8de077f7d16e1c71c
James Richardson <iagowynne@gmail.com>
2025-06-25 10:33:55 +0100

    feat[game]: add main scene and scene manager wrapper component

---
+++ website/src/app/game/Utils/sceneManager/index.ts
@@ -0,0 +1,5 @@
+import { SceneManagerContext } from "./SceneManagerContext";
+import SceneManagerProvider from "./SceneManagerProvider";
+import { Scene } from "./types";
+
+export { SceneManagerProvider as SceneManager, Scene, SceneManagerContext };
