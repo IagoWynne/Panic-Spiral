c298ad084c9144b56637351c3ccaae1bc6463be5
James Richardson <iagowynne@gmail.com>
2025-07-10 09:41:28 +0100

    feat[game]: add collision detection (#19)

--- website/src/app/game/Components/Tile.ts
+++ website/src/app/game/Components/Tile.ts
@@ -1,16 +1,22 @@
 import { Container, Sprite, Texture } from "pixi.js";
+import { CollisionZone } from "./CollisionZone";
 
 export class Tile extends Container {
-  constructor(alias: string, x: number, y: number, scale: number = 1) {
-    super();
+  public collisionZone?: CollisionZone;
 
+  constructor(alias: string, x: number, y: number, collidable = false) {
+    super();
     const sprite = new Sprite(Texture.from(alias));
     sprite.anchor.set(0.5);
-    sprite.scale.set(scale);
+    sprite.scale.set(0.5);
 
     this.x = x;
     this.y = y;
 
     this.addChild(sprite);
+
+    if (collidable) {
+      this.collisionZone = new CollisionZone(collidable, this);
+    }
   }
 }
--- website/src/app/game/Screens/Main/Ship/Ship.ts
+++ website/src/app/game/Screens/Main/Ship/Ship.ts
@@ -2,12 +2,25 @@ import { Container } from "pixi.js";
 import { Tile } from "../../../Components";
 
 export class Ship extends Container {
+  private _floor = new Container();
+  public walls = new Container();
+
   constructor() {
     super();
 
     for (let i = 0; i < 8; i++) {
-      const tile = new Tile(`floor-grey-${i}`, 32 + i * 32, 32, 0.5);
-      this.addChild(tile);
+      const tile = new Tile(`floor-grey-${i}`, 32 + i * 32, 32);
+      this._floor.addChild(tile);
     }
+
+    for (let i = 0; i < 7; i++) {
+      const wall = new Tile(`wall-grey-${i}`, 32 + i * 32, 128, true);
+      this.walls.addChild(wall);
+    }
+
+    this.walls.addChild(new Tile("wall-grey-6", 200, 200, true));
+
+    this.addChild(this._floor);
+    this.addChild(this.walls);
   }
 }
