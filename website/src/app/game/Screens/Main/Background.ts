3663342d2a19fb5cd34cfa90fa2f683065d90784
James Richardson <iagowynne@gmail.com>
2025-08-01 14:51:09 +0100

    feat: background moves when the engine is not broken (#33)

---
+++ website/src/app/game/Screens/Main/Background.ts
@@ -0,0 +1,50 @@
+import { TilingSprite, Texture } from "pixi.js";
+import { SystemEvents } from "./Systems";
+import { SYSTEM_IDS } from "../../constants/Systems";
+import { MAIN } from "../../constants";
+
+export class Background extends TilingSprite {
+  private _componentId = "game-background";
+  private _moving: boolean = true;
+
+  constructor() {
+    super(Texture.from("pixelart_starfield_corona"));
+
+    this.addListeners();
+  }
+
+  private addListeners() {
+    SystemEvents.addSystemListener({
+      componentId: this._componentId,
+      system: SYSTEM_IDS.ENGINE,
+      systemEventType: "BREAKDOWN",
+      action: () => {
+        this._moving = false;
+      },
+    });
+
+    SystemEvents.addSystemListener({
+      componentId: this._componentId,
+      system: SYSTEM_IDS.ENGINE,
+      systemEventType: "REPAIRED",
+      action: () => {
+        this._moving = true;
+      },
+    });
+  }
+
+  public resize(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public update() {
+    if (this._moving) {
+      this.tilePosition.x -= MAIN.BACKGROUND.BASE_MOVEMENT_RATE;
+    }
+  }
+
+  public cleanup() {
+    SystemEvents.removeSystemListener(this._componentId, SYSTEM_IDS.ENGINE);
+  }
+}
---
+++ website/src/app/game/constants/Main/Background.ts
@@ -0,0 +1,1 @@
+export const BASE_MOVEMENT_RATE = 0.2;
